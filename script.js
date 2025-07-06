774c49e47911f5f64606164ff2b2845c03fc0953
AbhayGhorela <abhayghorela7@gmail.com>
2025-07-06 18:57:47 +0530

    project is done

--- script.js
+++ script.js
@@ -2,57 +2,81 @@ const writeTodo = document.querySelector(".todo-container .add-input");
 const addbtn = document.querySelector(".add-btn");
 const liContainer = document.querySelector(".task-list-container ul");
 
+let tasks = [];
 
-function addTodo(){
-    if (writeTodo.value != "") {
-    const li = document.createElement("li");
-    const todo =document.createTextNode(writeTodo.value)
-    const checkBox = document.createElement('i')
-    const remove = document.createElement('i')
-    checkBox.classList.add('bx','bx-radio-circle','checkbox')
-    remove.classList.add('bx','bx-trash-x','remove')
-    li.appendChild(checkBox)
-    li.appendChild(todo); 
-    li.appendChild(remove)
-    liContainer.appendChild(li);
-    writeTodo.value = "";
-    getData()
+window.addEventListener('DOMContentLoaded',()=>{
+    let stored = localStorage.getItem('tasks')
+    tasks = stored?JSON.parse(stored):[];
+    renderTasks()
+})
 
-    // checked todo
-    li.addEventListener('click',()=>{
-        li.classList.toggle('checked')
-        const isChecked = li.classList.contains('checked')
-        checkBox.classList.toggle('bx-radio-circle',!isChecked)
-        checkBox.classList.toggle('bx-check-circle',isChecked)
-        getData()
-    })
-
-    // remove todo
-    remove.addEventListener('click',()=>{
-        li.remove()
-        getData()
-    })
-  }else{
-    alert("Dont give empty String in the todo!!!")
-  }
-}
-
-addbtn.addEventListener("click", () => {
-  addTodo()
-});
-
+addbtn.addEventListener('click',addTodo)
 window.addEventListener('keydown',(e)=>{
     if(e.key == 'Enter'){
         addTodo()
     }
 })
 
+function addTodo(){
+    const taskText = writeTodo.value.trim()
+    if(!taskText) return alert(`Don't give empty string in the todo!!!`)
 
-function getData(){
-    localStorage.setItem('data',liContainer.innerHTML)
+        const newTask = {
+            text:taskText,
+            completed:false,
+        };
+
+        tasks.push(newTask);
+        saveTasks()
+        renderTasks();
+        writeTodo.value = '';
+
 }
 
-function showData(){
-    liContainer.innerHTML = localStorage.getItem("data")
+
+function renderTasks(){
+
+    liContainer.innerHTML = ''
+
+    tasks.forEach((task,index)=>{
+        const li = document.createElement("li");
+        const todoText =document.createTextNode(task.text);
+        const checkBox = document.createElement('i');
+        const remove = document.createElement('i');
+
+        checkBox.classList.add("bx", task.completed ? "bx-check-circle" : "bx-radio-circle", "checkbox");
+        remove.classList.add("bx", "bx-trash-x", "remove");
+
+
+        if(task.completed) li.classList.add('checked')
+        li.appendChild(checkBox)
+        li.appendChild(todoText); 
+        li.appendChild(remove)
+        liContainer.appendChild(li);
+
+        checkBox.addEventListener('click',()=>{
+            tasks[index].completed = !tasks[index].completed;
+            saveTasks();
+            renderTasks();
+        });
+
+
+        remove.addEventListener('click',()=>{
+            tasks.splice(index,1);
+            saveTasks()
+            renderTasks()
+        })
+
+    })
+
+    
+    
+}  
+
+
+
+
+function saveTasks(){
+    localStorage.setItem('tasks',JSON.stringify(tasks))
 }
 
